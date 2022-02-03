7c1fce99cac024922d6233c63b8c53e64a50932a
udaylad77 <udaylad77@gmail.com>
2022-02-03 15:08:26 +0530

    first

---
+++ ecommerce/store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Customer, Order, Product, ShippingAddress, OrderItem
+
+# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(ShippingAddress)
+admin.site.register(OrderItem)
